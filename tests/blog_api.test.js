64177ea8893696aa6798abdf8c98bbb431fd9812
merinshaji-18 <merin.shaji.10e@gmail.com>
2025-05-31 23:16:36 +0530

    Complete Exercise 4.12: Verify 400 Bad Request for missing title/url

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -12,25 +12,15 @@ const api = supertest(app) // Wrapper for making HTTP requests
 // Initialize the database before each test
 beforeEach(async () => {
   await Blog.deleteMany({}) // Clear the Blogs collection
-  // console.log('cleared test database')
-
-  // Simpler way to insert initial blogs using Mongoose's insertMany
   await Blog.insertMany(helper.initialBlogs)
-  // console.log('inserted initial blogs')
-
-  // Alternative using Promise.all (more verbose but shows another way)
-  // const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
-  // const promiseArray = blogObjects.map(blog => blog.save())
-  // await Promise.all(promiseArray)
 })
 
-describe('when there are initially some blogs saved', () => {
+describe('when there are initially some blogs saved (GET operations)', () => { // Renamed slightly for clarity
   test('blogs are returned as json', async () => {
-    // console.log('entered test: blogs are returned as json')
     await api
       .get('/api/blogs')
-      .expect(200) // Expect HTTP status 200 OK
-      .expect('Content-Type', /application\/json/) // Expect Content-Type header to be application/json
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
   })
 
   test('all initial blogs are returned', async () => {
@@ -43,160 +33,101 @@ describe('when there are initially some blogs saved', () => {
     const titles = response.body.map(r => r.title)
     assert(titles.includes('React patterns'), 'Should contain "React patterns"')
   })
+
+  // Test for Exercise 4.9 (Verify 'id' property)
+  test('the unique identifier property of blogs is named id', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+
+    assert(blogs.length > 0, 'Should have at least one blog to test')
+
+    for (const blog of blogs) {
+      assert.ok(Object.prototype.hasOwnProperty.call(blog, 'id'), `Blog should have an 'id' property. Blog: ${JSON.stringify(blog)}`)
+      assert.strictEqual(Object.prototype.hasOwnProperty.call(blog, '_id'), false, `Blog should not have an '_id' property. Blog: ${JSON.stringify(blog)}`)
+    }
+  })
 })
 
-// tests/blog_api.test.js
-// ... (other requires and beforeEach, after blocks)
+describe('addition of a new blog (POST operations)', () => { // Renamed slightly for clarity
+  test('succeeds with valid data', async () => {
+    const newBlog = {
+      title: 'A Brand New Blog Post',
+      author: 'Test Author',
+      url: 'http://example.com/newblog',
+      likes: 15
+    }
 
-describe('when there are initially some blogs saved', () => {
-    test('blogs are returned as json', async () => {
-      // ... (existing test)
-      await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-    })
-  
-    test('all initial blogs are returned', async () => {
-      // ... (existing test)
-      const response = await api.get('/api/blogs')
-      assert.strictEqual(response.body.length, helper.initialBlogs.length)
-    })
-  
-    test('a specific blog title is within the returned blogs', async () => {
-      // ... (existing test)
-      const response = await api.get('/api/blogs')
-      const titles = response.body.map(r => r.title)
-      assert(titles.includes('React patterns'), 'Should contain "React patterns"')
-    })
-  
-    // New test for Exercise 4.9
-    test('the unique identifier property of blogs is named id', async () => {
-      const response = await api.get('/api/blogs')
-      const blogs = response.body
-  
-      // Check if at least one blog exists to test its properties
-      assert(blogs.length > 0, 'Should have at least one blog to test')
-  
-      // Verify that each blog object has an 'id' property
-      // and does NOT have an '_id' property (after toJSON transformation)
-      for (const blog of blogs) {
-        assert.ok(Object.prototype.hasOwnProperty.call(blog, 'id'), `Blog should have an 'id' property. Blog: ${JSON.stringify(blog)}`)
-        assert.strictEqual(Object.prototype.hasOwnProperty.call(blog, '_id'), false, `Blog should not have an '_id' property. Blog: ${JSON.stringify(blog)}`)
-      }
-    })
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(b => b.title)
+    assert(titles.includes('A Brand New Blog Post'), 'The new blog title should be in the database')
   })
-  describe('addition of a new blog', () => {
-    test('succeeds with valid data', async () => {
-      const newBlog = {
-        title: 'A Brand New Blog Post',
-        author: 'Test Author',
-        url: 'http://example.com/newblog',
-        likes: 15
-      }
-  
-      await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201) // Expect '201 Created'
-        .expect('Content-Type', /application\/json/)
-  
-      // Verify the database state
-      const blogsAtEnd = await helper.blogsInDb()
-      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-  
-      const titles = blogsAtEnd.map(b => b.title)
-      assert(titles.includes('A Brand New Blog Post'), 'The new blog title should be in the database')
-    })
-  
-    test('fails with status code 400 if title is missing', async () => {
-      // This test will be more relevant for exercise 4.12,
-      // but it's good to think about validation early.
-      // For now, we'll assume our controller might do basic validation or rely on Mongoose.
-      const newBlogWithoutTitle = {
-        author: 'Anonymous',
-        url: 'http://example.com/notitle',
-        likes: 5
-      }
-  
-      await api
-        .post('/api/blogs')
-        .send(newBlogWithoutTitle)
-        .expect(400) // Expect '400 Bad Request'
-  
-      const blogsAtEnd = await helper.blogsInDb()
-      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length, 'Blog count should not increase if title is missing')
-    })
-  
-    test('fails with status code 400 if url is missing', async () => {
-      // Similar to the title test, primarily for 4.12.
-      const newBlogWithoutUrl = {
-        title: 'A Blog With No URL',
-        author: 'Anonymous',
-        likes: 5
-      }
-  
-      await api
-        .post('/api/blogs')
-        .send(newBlogWithoutUrl)
-        .expect(400)
-  
-      const blogsAtEnd = await helper.blogsInDb()
-      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length, 'Blog count should not increase if URL is missing')
-    })
+
+  // Tests for Exercise 4.10 / 4.12 (Validating missing title/url)
+  test('fails with status code 400 if title is missing', async () => {
+    const newBlogWithoutTitle = {
+      author: 'Anonymous',
+      url: 'http://example.com/notitle',
+      likes: 5
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlogWithoutTitle)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length, 'Blog count should not increase if title is missing')
   })
-  describe('addition of a new blog', () => {
-    test('succeeds with valid data', async () => {
-      // ... (existing test)
-      const newBlog = { title: 'A Brand New Blog Post', author: 'Test Author', url: 'http://example.com/newblog', likes: 15 }
-      await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/)
-      const blogsAtEnd = await helper.blogsInDb()
-      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-      const titles = blogsAtEnd.map(b => b.title)
-      assert(titles.includes('A Brand New Blog Post'))
-    })
-  
-    test('fails with status code 400 if title is missing', async () => {
-      // ... (existing test)
-      const newBlogWithoutTitle = { author: 'Anonymous', url: 'http://example.com/notitle', likes: 5 }
-      await api.post('/api/blogs').send(newBlogWithoutTitle).expect(400)
-      const blogsAtEnd = await helper.blogsInDb()
-      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
-    })
-  
-    test('fails with status code 400 if url is missing', async () => {
-      // ... (existing test)
-      const newBlogWithoutUrl = { title: 'A Blog With No URL', author: 'Anonymous', likes: 5 }
-      await api.post('/api/blogs').send(newBlogWithoutUrl).expect(400)
-      const blogsAtEnd = await helper.blogsInDb()
-      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
-    })
-  
-    // New test for Exercise 4.11
-    test('if likes property is missing, it defaults to 0', async () => {
-      const newBlogWithoutLikes = {
-        title: 'Blog With No Likes Property',
-        author: 'Author Missing Likes',
-        url: 'http://example.com/nolikesprop'
-        // 'likes' property is intentionally omitted
-      }
-  
-      const response = await api
-        .post('/api/blogs')
-        .send(newBlogWithoutLikes)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-  
-      // Verify the 'likes' property of the returned blog object
-      assert.strictEqual(response.body.likes, 0, 'Likes should default to 0 in the response')
-  
-      // Optionally, verify in the database as well for robustness
-      const blogsAtEnd = await helper.blogsInDb()
-      const addedBlog = blogsAtEnd.find(blog => blog.title === 'Blog With No Likes Property')
-      assert.ok(addedBlog, 'The new blog should be found in the database')
-      assert.strictEqual(addedBlog.likes, 0, 'Likes should default to 0 in the database')
-    })
+
+  test('fails with status code 400 if url is missing', async () => {
+    const newBlogWithoutUrl = {
+      title: 'A Blog With No URL',
+      author: 'Anonymous',
+      likes: 5
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlogWithoutUrl)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length, 'Blog count should not increase if URL is missing')
   })
+
+  // Test for Exercise 4.11 (Default likes to 0)
+  test('if likes property is missing, it defaults to 0', async () => {
+    const newBlogWithoutLikes = {
+      title: 'Blog With No Likes Property',
+      author: 'Author Missing Likes',
+      url: 'http://example.com/nolikesprop'
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlogWithoutLikes)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual(response.body.likes, 0, 'Likes should default to 0 in the response')
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const addedBlog = blogsAtEnd.find(blog => blog.title === 'Blog With No Likes Property')
+    assert.ok(addedBlog, 'The new blog should be found in the database')
+    assert.strictEqual(addedBlog.likes, 0, 'Likes should default to 0 in the database')
+  })
+})
+
+// You might want to add describe blocks for DELETE, PUT operations later
+
 after(async () => {
   await mongoose.connection.close()
 })
