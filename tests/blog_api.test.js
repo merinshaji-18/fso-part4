d202da8d6293912f0515724a0012ff6050a4a27a
merinshaji-18 <merin.shaji.10e@gmail.com>
2025-05-31 22:53:20 +0530

    Complete Exercise 4.9: Verify 'id' property and transform MongoDB '_id'

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -45,7 +45,49 @@ describe('when there are initially some blogs saved', () => {
   })
 })
 
+// tests/blog_api.test.js
+// ... (other requires and beforeEach, after blocks)
 
+describe('when there are initially some blogs saved', () => {
+    test('blogs are returned as json', async () => {
+      // ... (existing test)
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+  
+    test('all initial blogs are returned', async () => {
+      // ... (existing test)
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    })
+  
+    test('a specific blog title is within the returned blogs', async () => {
+      // ... (existing test)
+      const response = await api.get('/api/blogs')
+      const titles = response.body.map(r => r.title)
+      assert(titles.includes('React patterns'), 'Should contain "React patterns"')
+    })
+  
+    // New test for Exercise 4.9
+    test('the unique identifier property of blogs is named id', async () => {
+      const response = await api.get('/api/blogs')
+      const blogs = response.body
+  
+      // Check if at least one blog exists to test its properties
+      assert(blogs.length > 0, 'Should have at least one blog to test')
+  
+      // Verify that each blog object has an 'id' property
+      // and does NOT have an '_id' property (after toJSON transformation)
+      for (const blog of blogs) {
+        assert.ok(Object.prototype.hasOwnProperty.call(blog, 'id'), `Blog should have an 'id' property. Blog: ${JSON.stringify(blog)}`)
+        assert.strictEqual(Object.prototype.hasOwnProperty.call(blog, '_id'), false, `Blog should not have an '_id' property. Blog: ${JSON.stringify(blog)}`)
+      }
+    })
+  })
+  
+  // ... (rest of the file, including the 'after' block)
 // Close the database connection after all tests have run
 after(async () => {
   await mongoose.connection.close()
